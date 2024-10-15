b3cbd54441784cf20dde77dbf289e85d3c8a2e6f
mateormz <mateo.ramirez@utec.edu.pe>
2024-10-15 00:48:17 -0500

    insert

---
+++ BST/balance-bst.cpp
@@ -0,0 +1,35 @@
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<TreeNode*> A;
+    TreeNode* balanceBST(TreeNode* root) {
+        inorder(root);
+        return sortedArrayToBST(0, A.size() - 1);
+    }
+    void inorder(TreeNode* root) {
+        if (root == NULL) return;
+        inorder(root->left);
+        A.push_back(root);
+        inorder(root->right);
+    }
+    TreeNode* sortedArrayToBST(int start, int end) {
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;
+        TreeNode* root = A[mid];
+        root->left = sortedArrayToBST(start, mid - 1);
+        root->right = sortedArrayToBST(mid + 1, end);
+        return root;
+    }
+};
